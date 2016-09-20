5b66dc743f2cc41f20b9b88219291e08437d0157
kwangkuku <kwangkuku@gmail.com>
2016-09-20 15:17:50 +0700

    Add
    ADD DATA

---
+++ app/src/main/java/com/promptnow/econprice/fragment/oil/data_dummy/Dummy.java
@@ -0,0 +1,33 @@
+package com.promptnow.econprice.fragment.oil.data_dummy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Acer on 19/9/2559.
+ */
+
+public class Dummy {
+
+    public static Dummy newInstance(){
+        return new Dummy();
+    }
+    //0 ---> Bensin
+    //1 ---> Sohol95
+    //2 ---> Sohol96
+    //3 ---> E20
+    //4 ---> E85
+    //5 ---> Disel
+
+    public List<String> ptt = new ArrayList<>(Arrays.asList("31.44","22.25","23.26","21.29","17.83","23.23"));
+    public List<String> bangjak = new ArrayList<>(Arrays.asList("22.25","-","-","21.81","17.18","23.44"));
+    public List<String> shell = new ArrayList<>(Arrays.asList("23.26","22.90","23.60","-","17.44","23.54"));
+    public List<String> esso = new ArrayList<>(Arrays.asList("21.29","23.12","24.11","21.61","-","23.09"));
+
+
+
+
+
+
+}
