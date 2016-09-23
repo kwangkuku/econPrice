d8310061950d499d03a50d29f4a393ad6c1f7445
Whan <sweet.whankungz@hotmail.com>
2016-09-23 09:51:56 +0700

    edit tab

--- app/src/main/java/com/promptnow/econprice/fragment/lottery/PageFragmentLotto.java
+++ app/src/main/java/com/promptnow/econprice/fragment/lottery/PageFragmentLotto.java
@@ -36,7 +36,7 @@ public class PageFragmentLotto extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.checklottery, container, false);
-        
+
 
         return view;
     }
